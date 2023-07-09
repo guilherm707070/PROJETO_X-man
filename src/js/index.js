1aa5b7d7e1a0411d737bb4d58c149e80cfc4222c
guilherm707070 <138339264+guilherm707070@users.noreply.github.com>
2023-07-09 15:27:16 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,41 @@
+const personagens = document.querySelectorAll('.personagem');
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+
+        if(window.innerWidth<450 ){
+            window.scrollTo({top:0,behavior: "smooth"});
+        }
+
+        removerseleçãodopesonagem();
+
+        personagem.classList.add('selecionado');
+
+        alteraImagemPersonagemGrande(personagem);
+
+       AlteraNomeDoPesonagemGrande(personagem);
+        
+       AlteraDescricaoDoPersonagem(personagem);
+
+       })
+})
+
+function AlteraDescricaoDoPersonagem(personagem) {
+    const descricãoPersonagem = document.getElementById('descricao-personagem');
+    descricãoPersonagem.innerText = personagem.getAttribute('data-description');
+}
+
+function AlteraNomeDoPesonagemGrande(personagem) {
+    const nomePersonagem = document.getElementById('nome-personagem');
+    nomePersonagem.innerText = personagem.getAttribute('data-name');
+}
+
+function alteraImagemPersonagemGrande(personagem) {
+    const imagempersonagemgrande = document.querySelector('.personagem-grande');
+    const idpersonagem = personagem.attributes.id.value;
+    imagempersonagemgrande.src = `/src/imagens/card-${idpersonagem}.png`;
+}
+
+function removerseleçãodopesonagem() {
+    const personagemselecionado = document.querySelector('.selecionado');
+    personagemselecionado.classList.remove('selecionado');
+}
